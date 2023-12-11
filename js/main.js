2c66040d69aed9855a1f5c08bade5537e11cdd96
Ruslan <ruslan.kl@proj.com>
2023-12-11 15:06:16 +0200

    added Home Page body,added breadcrumbs,title,achievements hover

--- js/main.js
+++ js/main.js
@@ -3,3 +3,11 @@ $(document).ready(function(){
       $(".wrapper").toggleClass("active")
     })
 });
+$(document).ready(function(){
+    $('[data-toggle="popover"]').popover({
+        placement : 'right',
+		trigger : 'hover',
+        html : true,
+        content : '<img src="images/icons/mentor.png" width="50px"><img src="images/icons/school.png" width="50px">'
+    });
+});
