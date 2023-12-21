dbd93d743fff8061f72f38491ca201ebf0d3f989
Ruslan <ruslan.kl@proj.com>
2023-12-21 19:42:56 +0200

    add notification dropdown

--- js/main.js
+++ js/main.js
@@ -1,13 +1,34 @@
-$(document).ready(function(){
-    $(".hamburger").click(function(){
-      $(".wrapper").toggleClass("active")
+var down = false;
+
+$(document).ready(function () {
+    $(".hamburger").click(function () {
+        $(".wrapper").toggleClass("active")
     })
 });
-$(document).ready(function(){
+$(document).ready(function () {
     $('[data-toggle="popover"]').popover({
-        placement : 'right',
-		trigger : 'hover',
-        html : true,
-        content : '<img src="images/icons/mentor.png" width="50px"><img src="images/icons/school.png" width="50px">'
+        placement: 'right',
+        trigger: 'hover',
+        html: true,
+        content: '<img src="images/icons/mentor.png" width="50px"><img src="images/icons/school.png" width="50px">'
     });
 });
+
+
+function toggleNotify(){
+    var box = document.getElementById('box');
+	if (down) {
+        box.style.height = '0px';
+        box.style.opacity = 0;
+        down = false;
+        box.style.cursor = 'default'
+    }
+    else {
+        box.style.height = '315px';
+        box.style.opacity = 1;
+        down = true;
+        document.getElementById('badge-bell').remove();
+        document.getElementById('notify_counter').innerHTML = " 0";
+    }
+}
+
